335b94b864eb5f8cd0e9e376f06f04cab107d564
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-01-14 18:44:44 -0300

    Resolução do desafio

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: lenght(list, 0)
+
+  defp lenght([], acc) do
+    acc
+  end
+
+  defp lenght([_head | tail], acc) do
+    acc = acc + 1
+    lenght(tail, acc)
   end
 end
